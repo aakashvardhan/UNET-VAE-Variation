7ffab97bcd9e011472fcb8b5a47646d18f82f213
aakashv100 <vardhan.aakash1@gmail.com>
2023-12-07 21:41:31 +0400

    Add channel expansion method in DecoderMiniBlock

--- U-Net/models/decoder_mini_block.py
+++ U-Net/models/decoder_mini_block.py
@@ -11,4 +11,16 @@ class DecoderMiniBlock(LightningModule):
         self.dropout = nn.Dropout2d(dropout)
         self.double_conv2d = DoubleConv(in_channels, out_channels)
         
+        if ce_method == "upsample":
+            self.ce = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        elif ce_method == "transposed_convolution":
+            self.ce = nn.ConvTranspose2d(in_channels, out_channels // 2, kernel_size=2, stride=2)
+        else:
+            raise Exception("Invalid Channel Expansion Method!")
         
+    def forward(self, x, skip):
+        x = self.ce(x)
+        x = torch.cat([x, skip], dim=1)
+        x = self.double_conv2d(x)
+        x = self.dropout(x)
+        return x
