e08388ac7d25e70d346a6bcfe1b5bc173ae04be8
aakashv100 <vardhan.aakash1@gmail.com>
2023-12-23 08:56:36 +0400

    Refactor dice_loss.py to improve readability and add comments

--- U-Net/dice_loss.py
+++ U-Net/dice_loss.py
@@ -7,29 +7,30 @@ from lightning import LightningModule
 
 
 class DiceLoss(LightningModule):
-    # Multi class Dice Loss for Image Segmentation using softmax
     def __init__(self, config):
         super().__init__()
         self.config = config
         
     def forward(self, y_pred, y_true):
-        # y_pred: (N, C, H, W)
-        # y_true: (N, H, W)
+        # Ensure y_true is of type torch.long for F.one_hot
+        if y_true.dtype != torch.long:
+            y_true = y_true.long()  # Convert y_true to long if it's not already
+
+        # Apply softmax to y_pred if required
         prob = y_pred
         if self.config['softmax_dim'] is not None:
             prob = nn.Softmax(dim=self.config['softmax_dim'])(y_pred)
-        # y_pred = F.softmax(y_pred, dim=self.config['softmax_dim'])
+
+        # Convert y_true to one-hot encoding
         y_true = F.one_hot(y_true, num_classes=self.config['num_classes']).permute(0, 3, 1, 2).float()
-        # y_true: (N, C, H, W)
-        
-        # dice loss
+
+        # Calculate dice loss
         numerator = 2 * torch.sum(prob * y_true, dim=(2, 3))
         denominator = torch.sum(prob ** 2 + y_true ** 2, dim=(2, 3))
         dice_loss = 1 - (numerator + 1) / (denominator + 1)
         
         return dice_loss.mean()
-    
-    
+
 
 # y_pred = torch.tensor([[[[0.7, 0.3], [0.4, 0.6]], [[0.3, 0.7], [0.6, 0.4]]]])
 # y_true = torch.tensor([[[0, 1], [1, 0]]])
