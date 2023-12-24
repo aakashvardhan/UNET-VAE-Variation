a87776375ac5fef9698f575a081dae5a48332098
aakashv100 <vardhan.aakash1@gmail.com>
2023-12-24 15:16:57 +0400

    Refactor callbacks.py for U-Net model

--- U-Net/callbacks.py
+++ U-Net/callbacks.py
@@ -5,20 +5,19 @@ from utils import plot_test_example
 
 
 class ClassAccuracyLoss(pl.Callback):
-    def __init__(self,config):
+    def __init__(self, config):
         super().__init__()
         self.config = config
 
-    def on_train_epoch_end(self, trainer):
+    def on_train_epoch_end(self, trainer, *args, **kwargs):
         print(
-        f"\n Epoch: {trainer.current_epoch} | Train Loss: {trainer.callback_metrics['train_loss']:.5f} | Train Acc: {trainer.callback_metrics['train_acc']:.5f}"
+            f"\n Epoch: {trainer.current_epoch} | Train Loss: {trainer.callback_metrics['train_loss']:.5f} | Train Acc: {trainer.callback_metrics['train_acc']:.5f}"
         )
         
-    def on_validation_epoch_end(self, trainer):
+    def on_validation_epoch_end(self, trainer, *args, **kwargs):
         print(
             f"\n Epoch: {trainer.current_epoch} | Val Loss: {trainer.callback_metrics['val_loss']:.5f} | Val Acc: {trainer.callback_metrics['val_acc']:.5f}"
         )
-        
 class PlotExampleCallback(pl.Callback):
     def __init__(self, config, interval=5):
         super().__init__()
