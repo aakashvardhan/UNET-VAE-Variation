1c9dbeae98a25ee838749f2e74504dd2d1342b24
aakashv100 <vardhan.aakash1@gmail.com>
2023-12-24 12:30:53 +0400

    Update callbacks.py to print trainer metrics

--- U-Net/callbacks.py
+++ U-Net/callbacks.py
@@ -9,14 +9,14 @@ class ClassAccuracyLoss(pl.Callback):
         super().__init__()
         self.config = config
 
-    def on_train_epoch_end(self):
+    def on_train_epoch_end(self, trainer):
         print(
-        f"\n Epoch: {self.current_epoch} | Train Loss: {self.trainer.callback_metrics['train_loss']:.5f} | Train Acc: {self.trainer.callback_metrics['train_acc']:.5f}"
+        f"\n Epoch: {self.current_epoch} | Train Loss: {trainer.callback_metrics['train_loss']:.5f} | Train Acc: {trainer.callback_metrics['train_acc']:.5f}"
         )
         
-    def on_validation_epoch_end(self):
+    def on_validation_epoch_end(self, trainer):
         print(
-            f"\n Epoch: {self.current_epoch} | Val Loss: {self.trainer.callback_metrics['val_loss']:.5f} | Val Acc: {self.trainer.callback_metrics['val_acc']:.5f}"
+            f"\n Epoch: {self.current_epoch} | Val Loss: {trainer.callback_metrics['val_loss']:.5f} | Val Acc: {trainer.callback_metrics['val_acc']:.5f}"
         )
         
 class PlotExampleCallback(pl.Callback):
@@ -25,10 +25,10 @@ class PlotExampleCallback(pl.Callback):
         self.config = config
         self.interval = interval
         
-    def on_validation_epoch_end(self):
+    def on_validation_epoch_end(self, trainer):
         epoch = self.trainer.current_epoch
         if epoch % self.interval == 0:
             print(f"\n Plotting example image from validation set at epoch {epoch}")
-            plot_test_example(self.trainer.datamodule.val_dataloader())
+            plot_test_example(trainer.datamodule.val_dataloader())
     
     
